a69887fd9eb25e26f725433e20260b0a88b53273
vipul-nair <43108515+vipul-nair@users.noreply.github.com>
2019-11-09 20:41:33 +0530

    Update LEVELXOR.C

--- LEVELXOR/src/LEVELXOR.C
+++ LEVELXOR/src/LEVELXOR.C
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void decrypt(char password[])
+{
+    char key[]="12345678";
+    char enc[]="C]\\@GYXL";
+    int i;
+    int len=strlen(enc);
+    for (i=0;i<=len;i++)
+    {
+        enc[i]=enc[i] ^ key[i]; 
+    }
+    if(strcmp(password,enc)==0)
+    {
+        printf("ACCESS GRANTED");
+    }
+    else{printf("WRONG PASSWORD");}
+
+}
+int main(int argc,char *argv[])
+{
+    if(argc!=2)
+    {
+        printf("USAGE:LEVELXOR.exe enter_Pass_here");
+        exit(1);
+    }
+
+    decrypt(argv[1]);
+
+    return 0;
+}
 
